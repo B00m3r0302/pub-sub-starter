b25d217a2e54f20e4f932be0915aec05960471a1
B00m3r0302 <hudson.t.woomer@gmail.com>
2026-03-12 10:47:51 -0400

    first exchange created

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -27,6 +30,12 @@ func main() {
 	}
 	defer mainChannel.Close()
 
+	pausedPlayingState, err := json.Marshal(routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pubsub.PublishJSON(mainChannel, routing.ExchangePerilDirect, routing.PauseKey, pausedPlayingState)
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
