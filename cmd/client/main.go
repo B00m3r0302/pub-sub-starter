c7c37ad292e1e589e056367bf0d24552a9cd071f
B00m3r0302 <hudson.t.woomer@gmail.com>
2026-03-12 15:30:53 -0400

    working client movements

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -18,24 +18,82 @@ func main() {
 	connectionString := "amqp://guest:guest@127.0.0.1:5672/"
 	connection, err := amqp.Dial(connectionString)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	defer connection.Close()
 	fmt.Println("Connected to RabbitMQ!")
 
 	username, err := gamelogic.ClientWelcome()
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	username = fmt.Sprintf("%s.%s", routing.PauseKey, username)
 
 	channel, queue, err := pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, username, routing.PauseKey, 1)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
 	log.Printf("connected to queue: %v on channel: %v", queue.Name, channel)
 
+	gamestate := gamelogic.NewGameState(username)
+
+	if 1 == 1 {
+		input := gamelogic.GetInput()
+
+		unitTypes := []string{"infantry", "cavalry", "artillery"}
+		locations := []string{"americas", "europe", "africa", "asia", "antarctica", "australia"}
+
+		found := false
+
+		command := input[0]
+		if command == "spawn" {
+			if len(input) != 3 {
+				fmt.Println("usage: spawn <type> <location>")
+				return
+			}
+			location := input[1]
+			unitType := input[2]
+
+			for _, listLocation := range locations {
+				if listLocation == unitType {
+					found = true
+				}
+			}
+
+			if !found {
+				fmt.Println("usage: spawn <type> <location>")
+				return
+			}
+
+			found = false
+
+			for _, listUnitType := range unitTypes {
+				if listUnitType == location {
+					found = true
+				}
+			}
+
+			if !found {
+				fmt.Println("usage: spawn <type> <location>")
+				return
+			}
+
+			err = gamestate.CommandSpawn(input)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		} else if command == "move" {
+			if len(input) != 3 {
+				fmt.Println("usage: move <from> <to>")
+				return
+			}
+		}
+	}
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
