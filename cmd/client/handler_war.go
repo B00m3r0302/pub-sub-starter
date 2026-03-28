12f3775951d8b541084f6f8d1f5cbca73590ae10
B00m3r0302 <hudson.t.woomer@gmail.com>
2026-03-28 10:53:44 -0400

    continue free use func

--- cmd/client/handler_war.go
+++ cmd/client/handler_war.go
@@ -6,12 +6,18 @@ import (
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+func publishGameLog(ch *amqp.Channel, exchange, username string, gl routing.GameLog) error {
+	key := fmt.Sprintf("%s.%s", routing.games)
+}
 
 func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.AckType {
 	return func(dw gamelogic.RecognitionOfWar) pubsub.AckType {
 		defer fmt.Print("> ")
-		outcome, _, _ := gs.HandleWar(dw)
+		outcome, winner, loser := gs.HandleWar(dw)
 
 		switch outcome {
 		case gamelogic.WarOutcomeNotInvolved:
@@ -19,8 +25,13 @@ func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub
 		case gamelogic.WarOutcomeNoUnits:
 			return pubsub.NackDiscard
 		case gamelogic.WarOutcomeYouWon:
+			log.Printf("%s won a war against %s", winner, loser)
+			return pubsub.Ack
+		case gamelogic.WarOutcomeOpponentWon:
+			log.Printf("%s won a war against %s", winner, loser)
 			return pubsub.Ack
 		case gamelogic.WarOutcomeDraw:
+			log.Printf("A war between %s and %s resulted in a draw", winner, loser)
 			return pubsub.Ack
 		}
 		log.Println("Some weird outcome happened")
